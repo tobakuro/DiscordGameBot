04e13f4523a0a7d6fae05e02a1e3760dcd69aeab
arcsino <173187432+arcsino@users.noreply.github.com>
2026-02-17 14:53:57 +0900

    APIの細かな修正と、ユーザーがボイスチャンネルに参加したときのイベントを追加

--- backend/discordapp/management/commands/runbot.py
+++ backend/discordapp/management/commands/runbot.py
@@ -33,21 +33,46 @@ class MyClient(discord.Client):
         print(f"We have logged in as {self.user}")
 
     async def on_member_join(self, member):
+        """メンバーがサーバーに参加したときのイベント"""
         for _ in range(3):
             r = requests.post(
-                f"http://127.0.0.1:8000/api/quiz-result/{member.id}/{member.display_name}/"  # ちがう
+                "http://127.0.0.1:8000/api/guild/add-member/",
+                headers={"Authorization": f"Token {self.token}"},
+                json={
+                    "guild_id": member.guild.id,
+                    "guild_name": member.guild.name,
+                    "discord_id": str(member.id),
+                    "username": member.display_name,
+                },
             )
-            if not r.status_code == 401:
+            if r.status_code == 200:
+                break
+
+    async def on_member_remove(self, member):
+        """メンバーがサーバーから退出したときのイベント"""
+        for _ in range(3):
+            r = requests.post(
+                "http://127.0.0.1:8000/api/guild/remove-member/",
+                headers={"Authorization": f"Token {self.token}"},
+                json={
+                    "guild_id": member.guild.id,
+                    "discord_id": str(member.id),
+                    "guild_name": member.guild.name,
+                    "username": member.display_name,
+                },
+            )
+            if r.status_code == 200:
                 break
 
     async def on_voice_state_update(self, member, before, after):
+        """ユーザーがボイスチャンネルに参加したときのイベント"""
         if before.channel is None and after.channel is not None:
             await after.channel.send(
                 f"{member.display_name} has joined the voice channel."
             )
 
 
-client = MyClient(intents=discord.Intents.default())
+client = MyClient(intents=discord.Intents.all())
 client.tree.add_command(quiz)
 client.tree.add_command(bluff_number)
 
